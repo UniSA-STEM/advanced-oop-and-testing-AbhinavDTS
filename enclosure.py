f9079e3d8fd796a10ab909c7dbdd728b071c3127
ABS <133721247+AbhinavDTS@users.noreply.github.com>
2025-11-24 12:57:43 +1030

    #This sections is here for ensuring that an animal that in is enclosure, making sure that the animal is healthy and makes sure that it matches the enclosure's allowed animla type before it is added

--- enclosure.py
+++ enclosure.py
@@ -16,3 +16,17 @@ class Enclosure:
         self.cleanliness = cleanliness
         self.animals = []
         self.animal_type = animal_type
+##This sections is here for ensuring that an animal that in is enclosure, making sure that the animal is healthy and makes sure that it matches the enclosure's allowed animla type before it is added
+
+    def add_animal(self, animal):
+        if not animal.is_healthy():
+            return f"{animal.name} cannot be moved; under treatment."
+
+        if self.animal_type is None:
+            self.animal_type = animal.category
+
+        if animal.category != self.animal_type:
+            return f"Cannot add {animal.name}. Only {self.animal_type} allowed."
+
+        self.animals.append(animal)
+        return f"{animal.name} added to {self.name}"
