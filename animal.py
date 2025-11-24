56fdcf5559d84fc3ec26058a0cb1c2041112f92c
ABS <133721247+AbhinavDTS@users.noreply.github.com>
2025-11-24 12:14:20 +1030

    The first part of the code represents a health issue for a paticular animal and talks abt details, severity and treament update

--- animal.py
+++ animal.py
@@ -5,7 +5,7 @@ ID: 110376072
 Username: shaay186
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
-
+# The first part of the code represents a health issue for a paticular animal and talks abt details, severity and treament update
 from datetime import date
 
 class HealthRecord:
