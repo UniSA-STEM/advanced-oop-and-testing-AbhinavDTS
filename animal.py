4403f512460a3c62ed07427aafdbf4f23c23f084
ABS <133721247+AbhinavDTS@users.noreply.github.com>
2025-11-24 12:22:25 +1030

    #Represents a bird in the zoo, extending the base animal class with bird-specific traits and behaviours.

--- animal.py
+++ animal.py
@@ -72,3 +72,11 @@ class Mammal(Animal):
 
     def make_sound(self):
         return f"{self.name} the {self.species} makes a mammal sound"
+
+#epresents a bird in the zoo, extending the base animal class with bird-specific traits and behaviours.
+class Bird(Animal):
+    def __init__(self, name, species, age, diet="Omnivore"):
+        super().__init__(name, species, age, category="Bird", diet=diet)
+
+    def make_sound(self):
+        return f"{self.name} the {self.species} chirps"
