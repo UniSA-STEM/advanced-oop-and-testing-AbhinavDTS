3be197863903ed116d9f933e670dd3c9ab4c20f8
ABS <133721247+AbhinavDTS@users.noreply.github.com>
2025-11-24 14:39:02 +1030

    #Represents a bird in the zoo, extending the base animal class with bird-specific traits and behaviours.

--- animal.py
+++ animal.py
@@ -71,12 +71,3 @@ class Animal:
 
 
 
-#Represents a reptile in the zoo, building on the base animal class with reptile-specific characteristics and behaviour.
-
-class Reptile(Animal):
-    def __init__(self, name, species, age, diet="Carnivore"):
-        super().__init__(name, species, age, category="Reptile", diet=diet)
-
-    def make_sound(self):
-        return f"{self.name} the {self.species} hisses"
-
