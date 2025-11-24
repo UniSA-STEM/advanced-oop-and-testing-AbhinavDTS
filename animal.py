6408e80eda0612cd8be3de9f321a524d9705c7fd
ABS <133721247+AbhinavDTS@users.noreply.github.com>
2025-11-24 12:13:29 +1030

    The first part of the code represents a health issue for a paticular animal and talks abt details, severity and treament update

--- animal.py
+++ animal.py
@@ -5,3 +5,19 @@ ID: 110376072
 Username: shaay186
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
+
+from datetime import date
+
+class HealthRecord:
+    def __init__(self, description, record_date, severity, treatment):
+        self.description = description
+        self.date = record_date
+        self.severity = severity
+        self.treatment = treatment
+        self.status = "Active"  # Active or Resolved
+
+    def resolve(self):
+        self.status = "Resolved"
+
+    def __repr__(self):
+        return f"HealthRecord(date={self.date}, severity={self.severity}, status={self.status}, desc='{self.description}')"
